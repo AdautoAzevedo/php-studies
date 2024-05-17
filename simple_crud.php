cb8393216f8954c545edf097939fc8e0013f00b7
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-05-16 21:30:17 -0300

    Added more methods to crud

--- simple_crud.php
+++ simple_crud.php
@@ -37,7 +37,37 @@
             } else {
                 "Error updating record: " . $conn->error;
             }
-            
+        }
 
+        //Read
+        $sql = "SELECT * FROM users";
+        $result = $conn->query($sql);
+
+        echo "<h2>Users</h2>";
+        echo "<table border='1'>";
+        echo "<tr><th>Id</th><th>Name</th><th>Email</th></tr>";
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr><td>".$row["id"]."</td><td>".$row["name"]."</td><td>".$row["email"]."</td></tr>";
+            } 
+        } else {
+            echo "0 results";
+        }
+
+        echo "</table>";
+
+        //Delete
+        if (isset($_POST['delete'])) {
+            $id = $_POST['id'];
+
+            $sql = "DELETE FROM users WHERE id=$id";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "Record deleted successfully";
+            } else {
+                echo "Error deleting record: " . $conn->error;
+            }
+            
         }
 ?>
